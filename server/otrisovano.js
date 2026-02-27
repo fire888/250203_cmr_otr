1c7c35bfaba4c0b37f46e2ed0968568effd6bdc2
vasilii <porokh.vs@gmail.com>
2026-02-27 11:57:04 +0300

    process

--- server/otrisovano.js
+++ server/otrisovano.js
@@ -3,6 +3,13 @@ const path = require('path')
 const app = express()
 const PORT = 8080
 
+// Добавляем заголовки
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(PORT, () => {
